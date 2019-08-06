b52b335c659fee7aa432b3a1ab0361e09a1c1796
merve.silk <secondstarfrmtherite@gmail.com>
2019-08-06 12:49:03 +0100

    Model created, mongoose added

---
+++ api/models/toDoListModel.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var TaskSchema = new Schema({
+  name: {
+    type: String,
+    required: "Please enter the name of your task",
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  status: {
+    type: [{
+      type: String,
+      enum: ["Pending", "Ongoing", "Completed"]
+    }],
+    default: ["Pending"]
+  }
+});
+
+module.exports = mongoose.model("Tasks", TaskSchema);
+
+
+
+
+//
+// 'use strict';
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+//
+//
+// var TaskSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: 'Kindly enter the name of the task'
+//   },
+//   Created_date: {
+//     type: Date,
+//     default: Date.now
+//   },
+//   status: {
+//     type: [{
+//       type: String,
+//       enum: ['pending', 'ongoing', 'completed']
+//     }],
+//     default: ['pending']
+//   }
+// });
+//
+// module.exports = mongoose.model('Tasks', TaskSchema);
