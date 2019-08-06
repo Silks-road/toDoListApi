00654434466861c23385f2b6ad11f7cf46943b27
merve.silk <secondstarfrmtherite@gmail.com>
2019-08-06 13:01:23 +0100

    Model file updated, Route added

---
+++ api/routes/toDoListRoute.js
@@ -0,0 +1,36 @@
+"use strict";
+module.exports = function(app) {
+  var toDoList = require("../controllers/toDoListController");
+
+  app.route("/tasks")
+    .get(toDoList.list_all_tasks)
+    .get(toDoList.create_a_task);
+
+  ap.route("/tasks/:taskId")
+    .get(toDoList.read_a_task)
+    .get(toDoList.update_a_task)
+    .get(toDoList.delete_a_task);
+};
+
+
+
+//
+//
+//
+//
+//
+// 'use strict';
+// module.exports = function(app) {
+//   var todoList = require('../controllers/todoListController');
+//
+//   // todoList Routes
+//   app.route('/tasks')
+//     .get(todoList.list_all_tasks)
+//     .post(todoList.create_a_task);
+//
+//
+//   app.route('/tasks/:taskId')
+//     .get(todoList.read_a_task)
+//     .put(todoList.update_a_task)
+//     .delete(todoList.delete_a_task);
+// };
